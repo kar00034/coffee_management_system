1a7837bdd71744edb4278b1611c305760512d081
jh <kar00034@naver.com>
2020-04-02 15:13:50 +0900

    lib 추가

--- main.py
+++ main.py
@@ -20,3 +20,12 @@ if __name__ == '__main__':
     # ma = management_system() # 권한추가
     # s = SaleMenu()
     app.exec_()
+
+"""
+select p.name, if(count(select salecnt from sale where DATE_FORMAT(date,'%Y %m') = '2020 02')=0,0,salecnt) from product p left join sale s on p.code = s.code GROUP by p.name;
+select name from product p join sale s on p.code=s.code where p.name = '팥빙수' and DATE_FORMAT(date,'%Y %m') = '2020 02' GROUP by name;
+select if(count(select salecnt from sale where DATE_FORMAT(date,'%Y %m') = '2020 02')=0,0,salecnt) from product p left join sale s on p.code = s.code GROUP by p.name;
+select salecnt from sale where DATE_FORMAT(date,'%Y %m') = '2020 02';
+
+lib = qt, pyqt5
+"""
--- ui_control/main_menu.py
+++ ui_control/main_menu.py
@@ -273,4 +273,6 @@ select p.name, if(count(select salecnt from sale where DATE_FORMAT(date,'%Y %m')
 select name from product p join sale s on p.code=s.code where p.name = '팥빙수' and DATE_FORMAT(date,'%Y %m') = '2020 02' GROUP by name;
 select if(count(select salecnt from sale where DATE_FORMAT(date,'%Y %m') = '2020 02')=0,0,salecnt) from product p left join sale s on p.code = s.code GROUP by p.name;
 select salecnt from sale where DATE_FORMAT(date,'%Y %m') = '2020 02';
+
+lib = qt, pyqt5
 """
