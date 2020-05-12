dc7900d0d41bcfb84c0b16335ebd59f1fc58ce1b
kar00034 <56991167+kar00034@users.noreply.github.com>
2020-05-12 15:01:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import readline
 from PyQt5.QtWidgets import QApplication
 from ui_control.login_ui import Login
+from ui_control.init_ui import Init
 
 if __name__ == '__main__':
     app = QApplication([])
     w = Login()
+#     I = Init()
     app.exec_()
